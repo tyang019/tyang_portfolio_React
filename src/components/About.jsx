b0aaee72f9fe2f9d283bb6fb449670537386c4e3
tyang019 <tyang019@fiu.edu>
2026-07-08 15:51:45 -0700

    Modify About.jsx for section_subtitle

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <section className="about-me" id="about">
       <h2 className="section__title section__title--about">Who I am</h2>
-      <p className="section__subtitle section__subtitle--about">Developer based out of Redding</p>
+      <p className="section__subtitle section__subtitle--about">Developer based out of Minneapolis, Minnesota</p>
       <div className="about-me__body">
         <p>I am a frontend-focused developer with a bachelor degree in Computer Science building responsive, user-friendly web applications using React, JavaScript, HTML, and CSS. I focus on writing clean, maintainable code and creating smooth user experiences through component-based design and modern web development practices.</p>
         <p>I turn ideas and designs into functional applications and continuously improve my skills through hands-on projects. I enjoy learning new technologies, and building projects that are both practical and visually engaging.</p>
